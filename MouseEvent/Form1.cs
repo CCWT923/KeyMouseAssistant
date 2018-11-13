0250896efdb2e6b81c53d80d63c90c202d066730
WuTao <ccwutao@gmail.com>
2018-11-14 02:05:09 +0800

    发送随机内容

--- MouseEvent/Form1.cs
+++ MouseEvent/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace MouseEvent
 {
@@ -15,6 +16,73 @@ namespace MouseEvent
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private Random _Random_TimerInterval;
+        private Random _Random_Choice;
+        private int _TimerInterval = 1000;
+        private int _Choice = 0;
+        private int _TimerIntervalMaxValue = 36000;
+        private int _TimerIntervalMinValue = 1000;
+        private int _SendCount = 0;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Trim() == "")
+            {
+                ShowInfo("文本框内容不能为空！");
+                return;
+            }
+
+            _Random_TimerInterval = new Random(DateTime.Now.Millisecond);
+            _TimerInterval = _Random_TimerInterval.Next(_TimerIntervalMinValue, _TimerIntervalMaxValue);
+            
+            if(timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                button1.Text = "Start";
+                ShowInfo("计时器停止。");
+            }
+            else
+            {
+                timer1.Enabled = true;
+                timer1.Interval = _TimerInterval;
+                button1.Text = "Stop";
+                ShowInfo("计时器启动，周期： " + (_TimerInterval / 1000) + " 秒。");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //滚动鼠标
+            //Win32API.mouse_event(0x800, 500, 500, int.Parse(textBox1.Text), 0);
+            string[] sentences = GetSentence(textBox1.Text);
+            //随机选择一个句子
+            string s = sentences[_Choice];
+            //发送
+            SendKeys.Send(s);
+            _SendCount++;
+            ShowInfo("发送 " + _SendCount.ToString() + " 次内容。");
+
+            //随机选择定时器周期和句子索引
+            _Random_TimerInterval = new Random(DateTime.Now.Millisecond);
+            _Random_Choice = new Random(DateTime.Now.Millisecond);
+            _TimerInterval = _Random_TimerInterval.Next(_TimerIntervalMinValue, _TimerIntervalMaxValue);
+            _Choice = _Random_Choice.Next(0, sentences.Length - 1);
+            timer1.Interval = _TimerInterval;
+            ShowInfo("当前时钟周期为："  + (_TimerInterval/1000).ToString() + "秒");
+            
+        }
+
+        private string[] GetSentence(string s)
+        {
+            string[] sentence = s.Split('\n');
+            return sentence;
+        }
+
+        private void ShowInfo(string s)
+        {
+            label1.Text = s;
         }
     }
 }
---
+++ MouseEvent/Win32API.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MouseEvent
+{
+    static class Win32API
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int flags, int dX, int dY, int button, int extraInfo);
+
+        const int MOUSEEVENTF_MOVE = 0x1; //鼠标移动
+        const int MOUSEEVENTF_LEFTDOWN = 0x2; //左键按下
+        const int MOUSEEVENTF_WHEEL = 0x800; //鼠标滚动
+    }
+}
